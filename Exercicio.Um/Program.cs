1cad15eaa097cbaa21590035cea7f56d8a3e2fd0
luangolembiewski <luangolembiewski74@gmail.com>
2022-03-02 20:47:35 -0300

    Exercicio 1

---
+++ Exercicio.Um/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio.Um
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor em R$: ");
+            string real = Console.ReadLine();
+            decimal realDec = 0.0m;
+            decimal precoReal = 0.1932m;
+
+            decimal.TryParse (real, out realDec);
+            decimal resultadoConvertido = realDec * precoReal;
+            Console.WriteLine($"Valor em $: {resultadoConvertido}");
+            Console.ReadLine();
+        }
+    }
+}
