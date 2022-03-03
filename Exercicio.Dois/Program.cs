3be4668e2af96578f11ea50dc0cba5ab5a9c955a
luangolembiewski <luangolembiewski74@gmail.com>
2022-03-02 21:00:01 -0300

    Exercicio 2

---
+++ Exercicio.Dois/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio.Dois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor em $: ");
+            string real = Console.ReadLine();
+            decimal realDec = 0.0m;
+            decimal precoDolar = 5.22m;
+
+            decimal.TryParse(real, out realDec);
+            decimal resultadoConvertido = realDec * precoDolar;
+            Console.WriteLine($"Valor em R$: {resultadoConvertido}");
+            Console.ReadLine();
+        }
+    }
+}
