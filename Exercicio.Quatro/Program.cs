3ecfbf0d4c8058760307c8217cf3e834cdc711af
luangolembiewski <luangolembiewski74@gmail.com>
2022-03-02 21:43:46 -0300

    Exercicio 4

---
+++ Exercicio.Quatro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio.Quatro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o ano de nascimento!");
+            string nascimentoStr = Console.ReadLine();
+            int idade;
+            int anoNascimento;
+            int anoAtual = DateTime.Today.Year;
+
+            int.TryParse(nascimentoStr, out anoNascimento);
+            idade = (anoAtual - anoNascimento);
+
+            Console.WriteLine($"A idade é {idade}");
+        }
+    }
+}
