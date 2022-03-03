761143ba1c04317601b03ee0b15f696488e9d3a4
luangolembiewski <luangolembiewski74@gmail.com>
2022-03-02 21:26:36 -0300

    Exercicio 3

---
+++ Exercicio.Tres/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio.Tres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a distancia percorrida em KM!");
+            string distanciaStr = Console.ReadLine();
+            Console.WriteLine("Informe o gasto total de combustível em Litros!");
+            string gastoStr = Console.ReadLine();
+            decimal distancia = 0.0m;
+            decimal gasto = 0.0m;
+            decimal media = 0.0m;
+
+            decimal.TryParse(distanciaStr, out distancia);
+            decimal.TryParse(gastoStr, out gasto);
+            media = distancia/gasto;
+
+            Console.WriteLine($"Seu veiculo fez uma média de {media}KM/Litro");
+        }
+    }
+}
