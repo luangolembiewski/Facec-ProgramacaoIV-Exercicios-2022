2fb1babeb732ce56eecc76a6a0ee44aea7a89c6f
luangolembiewski <luangolembiewski74@gmail.com>
2022-03-15 21:27:11 -0300

    Att

---
+++ Exercicio.Sete/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio.Sete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
