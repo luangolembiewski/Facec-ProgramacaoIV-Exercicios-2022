e7b54191c4d9fdbd393c7ebabff4d7d7b95b6754
Luan Golembiewski <luangolembiewski74@gmail.com>
2022-03-24 19:17:21 -0300

    att

--- Exercicio.Nove/Program.cs
+++ Exercicio.Nove/Program.cs
@@ -2,11 +2,12 @@
 
 namespace Exercicio.Nove
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(new Livro("Dois Irmãos", "Milton Hatoum", 99.90m).ToString());
+            Console.ReadLine();
         }
     }
 }
