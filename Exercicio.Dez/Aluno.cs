49f6a28ee343aea0a54af1222061c29772740887
luangolembiewski <luangolembiewski74@gmail.com>
2022-03-24 17:56:34 -0300

    att

---
+++ Exercicio.Dez/Aluno.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio.Dez
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Ra { get; set; }
+        public decimal Nota { get; set; }
+        public int Frequencia { get; set; }
+
+        public decimal Media(decimal nota)
+        {
+            decimal media = nota / 100;
+        return media;
+        }
+        public bool Aprovacao(int frequencia)
+        {
+            if (frequencia/25 < 0.40)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+    
+}
---
+++ Exercicio.Dez/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio.Dez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno()
+            {
+                Nome = "Luan",
+                Ra = 424,
+                Nota = 60,
+                Frequencia = 12
+            };
+            Console.WriteLine($"Nome: {aluno1.Nome} RA: {aluno1.Ra} Nota: {aluno1.Nota} Frequencia: {aluno1.Frequencia} Media: {aluno1.Media(aluno1.Nota)} => {aluno1.Aprovacao(aluno1.Frequencia)}");
+        }
+    }
+}
